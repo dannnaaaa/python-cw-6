26455134982e34dd3a74356bd2611c8c7c5b345e
dannnaaaa <142628992+dannnaaaa@users.noreply.github.com>
2023-09-06 19:34:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # write your code here
+person ={
+'name':'dana',
+'age':'16',
+'hobbies':['table tennis','playing video games','sleeping']
+}
+
+print(f" my name {person['name']} , {person['age'] }")
+
+person['age']= 0
+person['country'] = 'kuwait'
+print(person)
+print(len(person))
+
+person['hobbies'].append('designed')
+
+def check_hobbies(person):
+    if len(person)>= 3:
+        print('wow youre amazing')
+
+    else:
+        print("try harder")    
+
+check_hobbies(person)
